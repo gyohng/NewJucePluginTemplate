2a470333f7ba0a95a6b4441bbf34c59002bacc9c
George Yohng <1831537+gyohng@users.noreply.github.com>
2024-01-11 19:17:29 +0800

    windows builder

--- Plugin/windows-build_all.sh
+++ Plugin/windows-build_all.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+PROJECT_NAME="My Audio Plugin"
+
 # Pick your Microsoft Visual Studio version
-MSVS_VERSION="Visual Studio 16 2019"
-#MSVS_VERSION="Visual Studio 17 2022"
+MSVS_VERSION="Visual Studio 17 2022"
+
+MSVS_PATH="$("C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere" -latest -property installationPath)"
+MSVS_PATH="$(echo $MSVS_PATH | sed 's,\\,/,g')"
 
 MYDIR="$(dirname "$0")"
 MYDIR="$( cd "$MYDIR" ; pwd -P )"
@@ -12,32 +16,64 @@ MYDIR="$( cd "$MYDIR" ; pwd -P )"
 cd "$MYDIR"
 mkdir -p build
 
-# Building a 64-bit version
+CMAKEPATH=
+for f in \
+    "$MSVS_PATH/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe" \
+    "`which cmake`" \
+    "C:/Program Files/CMake/bin/cmake.exe" \
+; do
+    if [ -f "$f" ]; then
+        CMAKEPATH="$f"
+        break
+    fi
+done
+
+if [ "$CMAKEPATH" == "" ]; then
+    echo Please install CMake for Visual Studio
+    echo Alternatively install CMake from https://cmake.org/download/
+    exit -1
+fi
+
+cmake() {
+    "$CMAKEPATH" "$@"
+}
+
+for platform in win64 win32; do
+    cd "$MYDIR/build"
+    rm -rf $platform
+    mkdir -p $platform
+    cd $platform
+
+    cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=${MYDIR}/toolchain-clang-$platform.cmake" -DCMAKE_BUILD_TYPE=Release ../..
+    cmake --build . --config Release
+done
+
+# Creatin combined result
 cd "$MYDIR/build"
-rm -rf win64
-mkdir -p win64
-cd win64
+rm -rf output
+mkdir -p output
+cd output
 
-# using MSBuild/CL
-#cmake -G "${MSVS_VERSION}" -T host=x64 -A x64 ../..
-#cmake --build . --config Release
+if [ -f "../win64/${PROJECT_NAME}_artefacts/Release/AAX/${PROJECT_NAME}.aaxplugin/desktop.ini" ]; then
+    mkdir -p "${PROJECT_NAME}.aaxplugin/Contents/x64"
+    mkdir -p "${PROJECT_NAME}.aaxplugin/Contents/Win32"
 
-# using Clang (requires installation)
-cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=${MYDIR}/../tools/toolchain-clang-win64.cmake"  -DCMAKE_BUILD_TYPE=Release ../..
-cmake --build . --config Release
+    cp "../win64/${PROJECT_NAME}_artefacts/Release/AAX/${PROJECT_NAME}.aaxplugin/"desktop.ini "${PROJECT_NAME}.aaxplugin/"
+    cp "../win64/${PROJECT_NAME}_artefacts/Release/AAX/${PROJECT_NAME}.aaxplugin/"Plugin.ico "${PROJECT_NAME}.aaxplugin/"
+    cp "../win64/${PROJECT_NAME}_artefacts/Release/AAX/${PROJECT_NAME}.aaxplugin/"Contents/x64/* "${PROJECT_NAME}.aaxplugin/Contents/x64"
+    cp "../win32/${PROJECT_NAME}_artefacts/Release/AAX/${PROJECT_NAME}.aaxplugin/"Contents/Win32/* "${PROJECT_NAME}.aaxplugin/Contents/Win32"
+fi
 
-# ======= DELETE EVERYTHING BELOW THIS POINT IF YOU DON'T NEED x86 32-bit =======
+mkdir -p "${PROJECT_NAME}.vst3/Contents/x86_64-win"
+mkdir -p "${PROJECT_NAME}.vst3/Contents/x86-win"
 
-# Building a 32-bit version
-cd "$MYDIR/build"
-rm -rf win32
-mkdir -p win32
-cd win32
+cp "../win64/${PROJECT_NAME}_artefacts/Release/VST3/${PROJECT_NAME}.vst3/"desktop.ini "${PROJECT_NAME}.vst3/"
+cp "../win64/${PROJECT_NAME}_artefacts/Release/VST3/${PROJECT_NAME}.vst3/"Plugin.ico "${PROJECT_NAME}.vst3/"
+cp "../win64/${PROJECT_NAME}_artefacts/Release/VST3/${PROJECT_NAME}.vst3/"Contents/x86_64-win/* "${PROJECT_NAME}.vst3/Contents/x86_64-win"
+cp "../win32/${PROJECT_NAME}_artefacts/Release/VST3/${PROJECT_NAME}.vst3/"Contents/x86-win/* "${PROJECT_NAME}.vst3/Contents/x86-win"
 
-# using MSBuild/CL
-#cmake -G "${MSVS_VERSION}" -T host=x64 -A Win32 ../..
-#cmake --build . --config Release
+cp "../win64/${PROJECT_NAME}_artefacts/Release/VST/${PROJECT_NAME}.dll" "${PROJECT_NAME}64.dll"
+cp "../win32/${PROJECT_NAME}_artefacts/Release/VST/${PROJECT_NAME}.dll" "${PROJECT_NAME}32.dll"
 
-# using Clang (requires installation)
-cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=${MYDIR}/../tools/toolchain-clang-win32.cmake"  -DCMAKE_BUILD_TYPE=Release ../..
-cmake --build . --config Release
+cp "../win64/${PROJECT_NAME}_artefacts/Release/Standalone/${PROJECT_NAME}.exe" "${PROJECT_NAME}64.exe"
+cp "../win32/${PROJECT_NAME}_artefacts/Release/Standalone/${PROJECT_NAME}.exe" "${PROJECT_NAME}32.exe"
