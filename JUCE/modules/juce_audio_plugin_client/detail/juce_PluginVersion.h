b5af342724a23297bb5632d69e5f8f33894d190c
George Yohng <georgegitlab@oss3d.com>
2026-03-11 21:34:55 +0100

    Projucer compatibility patch

--- JUCE/modules/juce_audio_plugin_client/detail/juce_PluginVersion.h
+++ JUCE/modules/juce_audio_plugin_client/detail/juce_PluginVersion.h
@@ -1,7 +1,12 @@
 #pragma once
 
+// CMake builds generate JuceVersion.h and compile juce_audio_plugin_client_version.cpp,
+// which provides these globals. Projucer builds already have the macros defined
+// via JucePluginDefines.h, so we only need the indirection for CMake.
+#if __has_include("JuceVersion.h")
 extern unsigned g_JucePlugin_VersionCode;
 extern const char *g_JucePlugin_VersionString;
 
 #define JucePlugin_VersionCode g_JucePlugin_VersionCode
 #define JucePlugin_VersionString g_JucePlugin_VersionString
+#endif
--- JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_version.cpp
+++ JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_version.cpp
@@ -1,4 +1,10 @@
-#include "JuceVersion.h"
+#if __has_include("JuceVersion.h")
+ #include "JuceVersion.h"
+#elif __has_include("JucePluginDefines.h")
+ #include "JucePluginDefines.h"
+#else
+ #error "Neither JuceVersion.h (CMake) nor JucePluginDefines.h (Projucer) found"
+#endif
 
 extern unsigned g_JucePlugin_VersionCode;
 extern const char *g_JucePlugin_VersionString;
